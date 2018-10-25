34de30edf05b06e29454e7e519fba071d68890eb
festus <kiamfestoh@gmail.com>
2018-10-25 22:58:35 +0300

    add view interface

--- app/src/main/java/com/example/festus/notes/view/ListActivity.java
+++ app/src/main/java/com/example/festus/notes/view/ListActivity.java
@@ -1,7 +1,9 @@
-package com.example.festus.notes;
+package com.example.festus.notes.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.festus.notes.R;
 
 public class ListActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/com/example/festus/notes/view/ViewInterface.java
@@ -0,0 +1,16 @@
+package com.example.festus.notes.view;
+
+import com.example.festus.notes.data.Note;
+
+import java.util.List;
+
+/**
+ * Created by Festus Kiambi on 10/25/18.
+ */
+public interface ViewInterface {
+
+    void startDetailActivity(String dateAndTime, String message, int colorResource);
+
+    void setUpAdapterAndView(List<Note> listOfNotes);
+
+}
