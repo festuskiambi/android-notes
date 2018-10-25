b60b1a79d18401fb84aaece0ab7aaa398e262aa3
festus <kiamfestoh@gmail.com>
2018-10-25 22:48:12 +0300

    create a fake data source class and fake data source interface

---
+++ app/src/main/java/com/example/festus/notes/data/DataSourceInterface.java
@@ -0,0 +1,10 @@
+package com.example.festus.notes.data;
+
+import java.util.List;
+
+/**
+ * Created by Festus Kiambi on 10/25/18.
+ */
+public interface DataSourceInterface {
+    List<Note> getListOfNotes();
+}
---
+++ app/src/main/java/com/example/festus/notes/data/FakeDataSource.java
@@ -0,0 +1,67 @@
+package com.example.festus.notes.data;
+
+import com.example.festus.notes.R;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Created by Festus Kiambi on 10/25/18.
+ */
+public class FakeDataSource implements DataSourceInterface {
+
+    private static final int sizeOfCollection = 12;
+    private Random random;
+
+    private final String[] datesAndTimes = {
+            "6:30AM 06/01/2017",
+            "9:26PM 04/22/2013",
+            "2:01PM 12/02/2015",
+            "2:43AM 09/7/2018",
+    };
+
+    private final String[] messages = {
+            "Check out content like Fragmented Podcast to expose yourself to the knowledge, ideas, " +
+                    "and opinions of experts in your field",
+            "Look at Open Source Projects like Android Architecture Blueprints to see how experts" +
+                    " design and build Apps",
+            "Write lots of Code and Example Apps. Writing good Quality Code in an efficient manner "
+                    + "is a Skill to be practiced like any other.",
+            "If at first something doesn't make any sense, find another explanation. We all " +
+                    "learn/teach different from each other. Find an explanation that speaks to you."
+    };
+
+    private final int[] colours = {
+            R.color.BLUE,
+            R.color.RED,
+            R.color.GREEN,
+            R.color.YELLOW
+
+    };
+
+    public FakeDataSource() {
+    }
+
+    @Override
+    public List<Note> getListOfNotes() {
+
+        ArrayList<Note> listOfNotes = new ArrayList<>();
+        random = new Random();
+
+        for (int i = 0; i < 12; i++) {
+            int randOne = random.nextInt(4);
+            int randTwo = random.nextInt(4);
+            int randThree = random.nextInt(4);
+
+            Note note = new Note(
+                    datesAndTimes[randOne],
+                    messages[randTwo],
+                    colours[randThree]
+            );
+
+            listOfNotes.add(note);
+        }
+        return listOfNotes;
+    }
+}
