0bf5bb3a48ccd7dad9dacb60e1e847a58b447eb6
festus <kiamfestoh@gmail.com>
2018-11-02 13:53:15 +0300

    create new note viewmodel

---
+++ app/src/main/java/com/example/festus/notes/viewModel/NewNoteViewModel.java
@@ -0,0 +1,34 @@
+package com.example.festus.notes.viewModel;
+
+import android.arch.lifecycle.ViewModel;
+import android.os.AsyncTask;
+
+import com.example.festus.notes.data.Note;
+import com.example.festus.notes.data.NoteRepository;
+
+/**
+ * Created by Festus Kiambi on 11/2/18.
+ */
+public class NewNoteViewModel extends ViewModel {
+
+    private NoteRepository repository;
+
+    public NewNoteViewModel(NoteRepository repository) {
+        this.repository = repository;
+    }
+
+    public void addnewNoteToDatabase(Note note){
+        AddNoteTask task = new AddNoteTask();
+        task.execute(note);
+    }
+
+
+    private class AddNoteTask extends AsyncTask<Note, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Note... notes) {
+            repository.insertNote(notes[0]);
+            return null;
+        }
+    }
+}
