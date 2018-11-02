7502ad9cd22beb13846a477784c8d1c65e58a0ae
festus <kiamfestoh@gmail.com>
2018-11-02 10:04:26 +0300

    create base activity

---
+++ app/src/main/java/com/example/festus/notes/util/BaseActivity.java
@@ -0,0 +1,25 @@
+package com.example.festus.notes.util;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Festus Kiambi on 11/2/18.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public static void addFragment(
+            FragmentManager fragmentManager,
+            Fragment fragment,
+            int frameId,
+            String tag
+    ) {
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(frameId, fragment, tag);
+        fragmentTransaction.commit();
+    }
+
+
+}
